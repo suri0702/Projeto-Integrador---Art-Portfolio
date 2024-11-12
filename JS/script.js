ab2af284838ce8812d8a4d437fe4a5c14ac8dacb
suri0702 <danielletutox@hotmail.com>
2024-11-12 20:50:57 -0300

    Atualização Home - 2
    O arquivo CSS e HTML da página foram finalizados. Foi inserido o arquivo script.js com as animações para o menu mobile e para o slider de imagens

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,22 @@
 var navToggle = document.getElementById("nav-toggle-btn");
 var bar = gsap.timeline();
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+
+    })
+});
 
 bar.to('.bar-1', 0.5, {
     Attr: {
